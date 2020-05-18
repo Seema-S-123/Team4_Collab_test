e6e4840e7e6712bfee6dd731405afd51e41287af
Anuj Murali <40844059+AnujMurali@users.noreply.github.com>
2020-05-18 14:29:42 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "collab.h"
+
+int main() {
+  int a = 10;
+  int b = 5;
+  int c = 25;
+  printf ("Add: %d\n",add(a,b));
+  printf ("Subtract: %d\n",sub(a,b));
+  printf ("Multiply: %d\n",multiply(a,b));
+  
+  return 0;
+ }
